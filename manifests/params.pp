6290142696e5b2aac7c895aeaaf728ebb7e20dd0
Cristian Falcas <falcas@optymyze.com>
2017-03-28 15:30:50 +0300

    fix travis

--- manifests/params.pp
+++ manifests/params.pp
@@ -60,7 +60,8 @@ class candlepin::params {
     'TLS_ECDH_RSA_WITH_3DES_EDE_CBC_SHA',
     'TLS_ECDH_RSA_WITH_AES_128_CBC_SHA',
     'TLS_ECDH_RSA_WITH_AES_256_CBC_SHA',
-    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA']
+    'TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA',
+    ]
 
   $version = 'present'
   $wget_version = 'present'
