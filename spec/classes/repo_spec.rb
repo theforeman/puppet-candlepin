04d38c9ea13f4df580d6e7f81e1a10827faa7e0d
Eric D. Helms <ericdhelms@gmail.com>
2023-11-17 08:55:38 -0500

    Add baseurl parameter to repo class

--- spec/classes/repo_spec.rb
+++ spec/classes/repo_spec.rb
@@ -4,23 +4,48 @@ describe 'candlepin::repo' do
   on_supported_os.each do |os, os_facts|
     context "on #{os}" do
       let(:facts) { os_facts }
-      let(:params) do
-        { version: 'nightly' }
+
+      describe 'with nightly version' do
+        let(:params) do
+          { version: 'nightly' }
+        end
+
+        it { is_expected.to compile.with_all_deps }
+
+        it do
+          is_expected.to contain_yumrepo('candlepin')
+            .with_descr('Candlepin: an open source entitlement management system.')
+            .with_baseurl("https://yum.theforeman.org/candlepin/nightly/el#{facts[:os]['release']['major']}/\$basearch/")
+            .with_gpgkey(nil)
+            .with_gpgcheck(false)
+            .with_enabled(true)
+            .that_comes_before('Anchor[candlepin::repo]')
+        end
+
+        it { is_expected.to contain_anchor('candlepin::repo') }
       end
 
-      it { is_expected.to compile.with_all_deps }
+      describe 'with baseurl' do
+        let(:params) do
+          {
+            baseurl: "https://stagingyum.theforeman.org/candlepin/nightly/el#{facts[:os]['release']['major']}/\$basearch/"
+          }
+        end
 
-      it do
-        is_expected.to contain_yumrepo('candlepin')
-          .with_descr('Candlepin: an open source entitlement management system.')
-          .with_baseurl("https://yum.theforeman.org/candlepin/nightly/el#{facts[:os]['release']['major']}/\$basearch/")
-          .with_gpgkey(nil)
-          .with_gpgcheck(false)
-          .with_enabled(true)
-          .that_comes_before('Anchor[candlepin::repo]')
+        it { is_expected.to compile.with_all_deps }
+
+        it do
+          is_expected.to contain_yumrepo('candlepin')
+            .with_descr('Candlepin: an open source entitlement management system.')
+            .with_baseurl("https://stagingyum.theforeman.org/candlepin/nightly/el#{facts[:os]['release']['major']}/\$basearch/")
+            .with_gpgkey(nil)
+            .with_gpgcheck(false)
+            .with_enabled(true)
+            .that_comes_before('Anchor[candlepin::repo]')
+        end
+
+        it { is_expected.to contain_anchor('candlepin::repo') }
       end
-
-      it { is_expected.to contain_anchor('candlepin::repo') }
     end
   end
 end
