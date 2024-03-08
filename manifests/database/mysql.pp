881e02db7a982f1297ed8ec01ac7635089c85ae0
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-03-08 15:22:27 +0100

    Rewrite _candlepin_database.conf from ERB to EPP
    Like the tomcat changes, this also implements better secret handling.
    This bumps the required PostgreSQL module version to one that deals with
    Sensitive. puppetlabs-mysql is a soft dependency, but now 11.1.0 is the
    oldest compatible version.
    
    The specs are modified because .without_content(senstive()) doesn't
    work. Instead it now checks for the line ending to ensure it's rendered
    correctly.

--- manifests/database/mysql.pp
+++ manifests/database/mysql.pp
@@ -12,15 +12,27 @@ class candlepin::database::mysql (
   $db_port = pick($candlepin::db_port, 3306),
   $db_name = $candlepin::db_name,
   $db_user = $candlepin::db_user,
-  # TODO: use EPP instead of  ERB, as EPP handles Sensitive natively
-  $db_password = $candlepin::db_password.unwrap,
+  $db_password = $candlepin::_db_password,
   $enable_hbm2ddl_validate = $candlepin::enable_hbm2ddl_validate,
   # lint:endignore
 ) {
   assert_private()
 
+  $context = {
+    'db_dialect'              => $db_dialect,
+    'db_quartz_dialect'       => $db_quartz_dialect,
+    'db_driver'               => $db_driver,
+    'db_type'                 => $db_type,
+    'db_host'                 => $db_host,
+    'db_port'                 => $db_port,
+    'db_name'                 => $db_name,
+    'db_user'                 => $db_user,
+    'db_password'             => $db_password,
+    'enable_hbm2ddl_validate' => $enable_hbm2ddl_validate,
+  }
+
   concat::fragment { 'Mysql Database Configuration':
     target  => $candlepin_conf_file,
-    content => template('candlepin/_candlepin_database.conf.erb'),
+    content => epp('candlepin/_candlepin_database.conf.epp', $context),
   }
 }
