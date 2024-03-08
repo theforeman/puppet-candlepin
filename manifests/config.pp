b1fc056cc33b87c794ca30d5448716108f9028bc
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2024-03-08 14:17:40 +0100

    Convert server.xml from ERB to EPP
    Like with broker.xml this gives proper handling of sensitive values.

--- manifests/config.pp
+++ manifests/config.pp
@@ -25,9 +25,22 @@ class candlepin::config {
     group => $candlepin::group,
   }
 
+  $server_context = {
+    'module_name'         => $module_name,
+    'host'                => $candlepin::host,
+    'ssl_port'            => $candlepin::ssl_port,
+    'ciphers'             => $candlepin::ciphers,
+    'tls_versions'        => $candlepin::tls_versions,
+    'keystore_file'       => $candlepin::keystore_file,
+    'keystore_password'   => $candlepin::_keystore_password,
+    'keystore_type'       => $candlepin::keystore_type,
+    'truststore_file'     => $candlepin::truststore_file,
+    'truststore_password' => $candlepin::_truststore_password,
+  }
+
   file { '/etc/tomcat/server.xml':
     ensure  => file,
-    content => template('candlepin/tomcat/server.xml.erb'),
+    content => epp('candlepin/tomcat/server.xml.epp', $server_context),
     mode    => '0640',
     owner   => 'root',
     group   => $candlepin::group,
