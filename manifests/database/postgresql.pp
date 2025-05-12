theforeman/puppet-candlepin

commit ca771455713546b6eb216116d546526097d3e71c

Ensure glibc-langpack-en is always installed

manifests/database/postgresql.pp
@@ -51,11 +51,15 @@ class candlepin::database::postgresql (
     }
 
     include postgresql::client, postgresql::server
+
+    stdlib::ensure_packages(['glibc-langpack-en'])
+
     postgresql::server::db { $db_name:
       user     => $db_user,
       password => postgresql::postgresql_password($db_user, $db_password),
       encoding => 'utf8',
       locale   => 'en_US.utf8',
+      require  => Package['glibc-langpack-en'],
     }
   }
 

spec/setup_acceptance_node.pp
@@ -1,9 +1,5 @@
 $major = $facts['os']['release']['major']
 
-package { 'glibc-langpack-en':
-  ensure => installed,
-}
-
 class { 'candlepin::repo':
   version => pick(fact('candlepin_version'), 'nightly'),
   baseurl => fact('candlepin_baseurl'),

commit 3e7c0d7616238b4745e44b69132a9f3c678b995b

Avoid dependency cycles with glibc-langpack-en
By calling stdlib::ensure_packages() in the class itself the resource
gets tied to the class. If other packages also depend on the resource,
you can get dependency cycles.

In particular, foreman also does this and that causes a dependency
cycle.

By using a class that is included the resource "floats" in the catalog.
We then only ensure the package is installed somewhere before we create
the database.

Fixes: ca7714557135 ("Ensure glibc-langpack-en is always installed")

manifests/database/postgresql.pp
@@ -51,15 +51,14 @@ class candlepin::database::postgresql (
     }
 
     include postgresql::client, postgresql::server
-
-    stdlib::ensure_packages(['glibc-langpack-en'])
+    include candlepin::database::postgresql::encoding
 
     postgresql::server::db { $db_name:
       user     => $db_user,
       password => postgresql::postgresql_password($db_user, $db_password),
       encoding => 'utf8',
       locale   => 'en_US.utf8',
-      require  => Package['glibc-langpack-en'],
+      require  => Class['candlepin::database::postgresql::encoding'],
     }
   }
 

manifests/database/postgresql/encoding.pp
@@ -1,0 +1,5 @@
+# @summary Class to ensure the packages for encoding are installed
+# @api private
+class candlepin::database::postgresql::encoding {
+  stdlib::ensure_packages(['glibc-langpack-en'])
+}